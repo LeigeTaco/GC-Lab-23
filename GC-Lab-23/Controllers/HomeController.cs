0ea121a1dfcd34b7f38a15e7a53e55a5a0d8ede5
williamTwomey <ashketchup.pkmn@gmail.com>
2018-08-27 22:59:21 -0400

    All actions are here, views are on their way.

--- GC-Lab-23/Controllers/HomeController.cs
+++ GC-Lab-23/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GC_Lab_23.Models;
 
 namespace GC_Lab_23.Controllers
 {
@@ -25,6 +26,88 @@ namespace GC_Lab_23.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult NewItem()
+        {
+
+            return View();
+        }
+
+        public ActionResult SaveNewItem(Item newItem)
+        {
+            GCWebORM ORM = new GCWebORM();
+
+            ORM.Items.Add(newItem);
+            ORM.SaveChanges();
+
+            return View();
+        }
+
+        public ActionResult DeleteItem(int itemID)
+        {
+            GCWebORM ORM = new GCWebORM();
+            Item itemToDelete = ORM.Items.Find(itemID);
+            ORM.Items.Remove(itemToDelete);
+            try
+            {
+                ORM.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                ViewBag.ServerException = e;
+                return View("Error");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Error()
+        {
+
+            return View();
+        }
+
+        public ActionResult UpdateItem(int itemID)
+        {
+            GCWebORM ORM = new GCWebORM();
+            Item itemToEdit = ORM.Items.Find(itemID);
+            ViewBag.ItemToEdit = itemToEdit;
+
+            return View();
+        }
+
+        public ActionResult SaveUpdatedItem(Item updatedItem)
+        {
+            GCWebORM ORM = new GCWebORM();
+            Item oldItem = ORM.Items.Find(updatedItem.ItemID);
+
+            oldItem.Name = updatedItem.Name;
+            oldItem.Description = updatedItem.Description;
+            oldItem.Stock = updatedItem.Stock;
+            oldItem.Price = updatedItem.Price;
+
+            ORM.Entry(oldItem).State = System.Data.Entity.EntityState.Modified;
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        public ActionResult AddNewUser(string Username, string Password, string Email, string Phone)
+        {
+            GCWebORM ORM = new GCWebORM();
+            bool isAdmin = false;
+            Guid UserID = Guid.NewGuid();
+            ORM.Users.Add(new User(UserID, Username, Password, Email, Phone, isAdmin));
+            ORM.SaveChanges();
+
+            return View("Index");
         }
     }
 }
